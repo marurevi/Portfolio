3a149143c93bc28c52bdad55169abfb7dfb26301
marurevi <maryrev@hotmail.com>
2022-04-05 14:23:09 -0300

    Add menu button funcionality with js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+let menuButton = document.getElementById('menuBtn');
+const hiddenPart = document.getElementById('headline');
+const menuContainer = document.querySelector('nav');
+const menuItems = document.querySelectorAll ('.display-menu');
 
+
+function menuDisplay() {
+  menuButton.classList.add('hidden');
+  hiddenPart.classList.toggle('hidden');
+  menuContainer.style.display = 'flex';
+  menuContainer.style.flexDirection = 'column';
+  menuContainer.style.textDecoration = 'none';
+  menuContainer.style.padding = '100px 0 250px 0';
+  menuContainer.style.backgroundImage= 'url(./images/image_geometry_menu_1-1.svg), url(./images/image_geometry_menu_2-1.svg)'
+  menuContainer.style.backgroundPosition = 'top, bottom'  
+  menuItems.forEach(
+      item => item.classList.remove('hidden'));
+     
+ 
+}
+
+menuButton.addEventListener('click', menuDisplay)
