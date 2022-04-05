89a4e322853e792fe4e4946a0dd771802e16e8fe
marurevi <maryrev@hotmail.com>
2022-04-05 16:27:08 -0300

    Update js

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-let menuButton = document.getElementById('menuBtn');
+const menuButton = document.getElementById('menuBtn');
 const hiddenPart = document.getElementById('headline');
 const menuContainer = document.querySelector('nav');
-const menuItems = document.querySelectorAll ('.display-menu');
-
+const menuItems = document.querySelectorAll('.display-menu');
 
 function menuDisplay() {
   menuButton.classList.add('hidden');
@@ -11,9 +10,9 @@ function menuDisplay() {
   menuContainer.style.flexDirection = 'column';
   menuContainer.style.textDecoration = 'none';
   menuContainer.style.padding = '100px 0 250px 0';
-  menuContainer.style.backgroundImage= 'url(./images/image_geometry_menu_1-1.svg), url(./images/image_geometry_menu_2-1.svg)'
-  menuContainer.style.backgroundPosition = 'top, bottom'  
-  menuItems.forEach(item => item.classList.remove('hidden')); 
+  menuContainer.style.backgroundImage = 'url(./images/image_geometry_menu_1-1.svg), url(./images/image_geometry_menu_2-1.svg)';
+  menuContainer.style.backgroundPosition = 'top, bottom';
+  menuItems.forEach((item) => item.classList.remove('hidden'));
 }
 
-menuButton.addEventListener('click', menuDisplay)
+menuButton.addEventListener('click', menuDisplay);
