28df68688c558257be5ff9f7fddf6e3525846494
marurevi <maryrev@hotmail.com>
2022-04-06 00:36:46 -0300

    Creation of menuContainer class

--- script.js
+++ script.js
@@ -11,24 +11,21 @@ function menuQuit() {
   hiddenPart.classList.toggle('hidden');
   menuItems.forEach((item) => item.classList.add('hidden'));
   quitButton.classList.add('hidden');
-  menuContainer.removeAttribute('style');
+  menuContainer.classList.remove('menuContainer');
 }
 
 function menuDisplay() {
   menuButton.classList.add('hidden');
   hiddenPart.classList.toggle('hidden');
-  menuContainer.style.display = 'flex';
-  menuContainer.style.flexDirection = 'column';
-  menuContainer.style.textDecoration = 'none';
-  menuContainer.style.padding = '100px 0 250px 0';
-  menuContainer.style.backgroundImage = 'url(./images/image_geometry_menu_1-1.svg), url(./images/image_geometry_menu_2-1.svg)';
-  menuContainer.style.backgroundPosition = 'top, bottom';
+  menuContainer.classList.add('menuContainer');
   menuItems.forEach((item) => { item.classList.remove('hidden'); item.addEventListener('click', menuQuit); });
+  quitButton.classList.remove('hidden');
   quitButton.addEventListener('click', menuQuit);
 }
 
 function projectDisplay() {
   hiddenPart2.forEach((item) => { item.classList.add('hidden') });
 }
+
 projectBtn[0].addEventListener('click', projectDisplay);
 menuButton.addEventListener('click', menuDisplay);
