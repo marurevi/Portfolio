43ba7cc77847a89edaca7658594b794c27edf9a9
marurevi <maryrev@hotmail.com>
2022-04-07 17:17:47 -0300

    Add changes in submit formula

--- formValidation.js
+++ formValidation.js
@@ -1,10 +1,9 @@
 const email = document.getElementById('email');
 const msg = document.querySelector('small');
 
-email.addEventListener('input', function (event) {
-    if(email.value == email.value.toLowerCase()){
-        msg.textContent = "";
-    }else{
-        msg.textContent = "Please use lower case only.";
+email.addEventListener('submit', (event) => {
+  if(email.value !== email.value.toLowerCase()){
+    msg.textContent = "Please use lower case only.";
+    event.preventDefault();
     }
-})
+});
