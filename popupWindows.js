9ef5226f15c25dd5609c9f15a186fee4554f5de8
marurevi <maryrev@hotmail.com>
2022-04-08 13:49:25 -0300

    Add div container to englobe al new elements in js file

--- popupWindows.js
+++ popupWindows.js
@@ -17,10 +17,7 @@ class Card {
 const projects = [];
 for (let i = 1; i < 7; i += 1) {
   let cardNumb = `card${i}`;
-  cardNumb = new Card(`card${i}`, 'Project name goes here', `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-  tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-  do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-  exercitation ullamco laboris nisi.`, `./images/pj${i}.png`, ['HTML/CSS', 'Ruby on Rails', 'JavaScript'], 'www.google.com', 'www.github.com');
+  cardNumb = new Card(`card${i}`, 'Project name goes here', `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.`, `./images/pj${i}.svg`, ['HTML/CSS', 'Ruby on Rails', 'JavaScript'], 'www.google.com', 'www.github.com');
   projects.push(cardNumb);
   // console.log(cardNumb)
 }
@@ -31,30 +28,34 @@ for (let i = 1; i < 7; i += 1) {
 
 const btnWhite = document.querySelectorAll('.btnWhite');
 const popupContainer = document.getElementById('popupContainer');
-const exitBtn = document.createElement('div');
+const exitBtn = document.getElementById('exitBtn');
 
 function fillContainer(card) {
   // Create elements
+  const containerNewElements = document.createElement('div');
   const titleProject = document.createElement('h2');
   const technologiesContainer = document.createElement('ul');
   const picture = document.createElement('img');
   const description = document.createElement('p');
+  const buttonContainer = document.createElement('div');
   const linksBtnLive = document.createElement('button');
   const linksBtnSource = document.createElement('button');
 
   for (const i of projects) {
     if (i.name === card) {
     // fill and style each one.
-      exitBtn.setAttribute('id', '#exitButton');
-      exitBtn.innerHTML = '<button type="button"><img src="./images/Union.svg" alt=""></img></button>';
-      popupContainer.appendChild(exitBtn);
+      containerNewElements.className = 'containerNewElements';
+      popupContainer.appendChild(containerNewElements);
+      exitBtn.className = 'exitButton';
+      exitBtn.innerHTML = '<img src="./images/Union.svg" alt=""></img>';
+      containerNewElements.appendChild(exitBtn);
 
       titleProject.innerText = i.title;
       titleProject.className = 'titleProject';
-      popupContainer.appendChild(titleProject);
+      containerNewElements.appendChild(titleProject);
 
       technologiesContainer.className = 'technologiesContainer';
-      popupContainer.appendChild(technologiesContainer);
+      containerNewElements.appendChild(technologiesContainer);
 
       for (const t of i.technologies) {
         const technologies = document.createElement('li');
@@ -65,31 +66,36 @@ function fillContainer(card) {
 
       picture.setAttribute('src', `${i.featuredImage}`);
       picture.className = 'imgPopup';
-      popupContainer.appendChild(picture);
+      containerNewElements.appendChild(picture);
 
       description.className = 'textPopup';
       description.innerText = `${i.description}`;
-      popupContainer.appendChild(description);
+      containerNewElements.appendChild(description);
+
+      buttonContainer.className = 'buttonContainer';
+      containerNewElements.appendChild(buttonContainer);
 
       linksBtnLive.className = 'btnWhite';
       linksBtnLive.setAttribute('type', 'button');
       linksBtnLive.innerHTML = `<a class="txtlink" href=${i.linkLiveVersion}>See Live <span><img src="./images/btnUnion.svg" alt=""></span><a/>`;
-      popupContainer.appendChild(linksBtnLive);
+      buttonContainer.appendChild(linksBtnLive);
 
       linksBtnSource.className = 'btnWhite';
       linksBtnSource.innerText = 'See Live';
       linksBtnSource.setAttribute('type', 'button');
       linksBtnSource.innerHTML = `<a class="txtlink" href=${i.linkSource}>See source <span><img src="./images/btnGitHub.svg" alt=""></span><a/>`;
-      popupContainer.appendChild(linksBtnSource);
+      buttonContainer.appendChild(linksBtnSource);
 
       function exitPopup() {
         titleProject.remove();
         technologiesContainer.remove();
         picture.remove();
         description.remove();
+        buttonContainer.remove();
         linksBtnLive.remove();
         linksBtnSource.remove();
         exitBtn.remove();
+        containerNewElements.remove();
         popupContainer.classList.add('hidden');
       }
 
