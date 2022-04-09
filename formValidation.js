a8bafdfeb732522a0b3471368ea75aeac08836b1
marurevi <maryrev@hotmail.com>
2022-04-09 04:12:59 -0300

    Fix issues on js file and styling

--- formValidation.js
+++ formValidation.js
@@ -1,8 +1,8 @@
 const email = document.getElementById('email');
 const msg = document.querySelector('small');
-const form = document.getElementById('submitbtn');
+const form = document.getElementById('contact');
 
-email.addEventListener('submit', (event) => {
+form.addEventListener('submit', (event) => {
   if (email.value !== email.value.toLowerCase()) {
     msg.textContent = 'Please use lower case only.';
     email.style.border = '1px solid red';
