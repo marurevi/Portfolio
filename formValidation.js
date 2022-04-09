f93ef1413e963b9ac56b19c08450123a7e19b038
marurevi <maryrev@hotmail.com>
2022-04-09 02:29:09 -0300

    Add styling and error msg

--- formValidation.js
+++ formValidation.js
@@ -1,9 +1,14 @@
 const email = document.getElementById('email');
 const msg = document.querySelector('small');
 
-email.addEventListener('submit', (event) => {
+email.addEventListener('input', (event) => {
   if(email.value !== email.value.toLowerCase()){
     msg.textContent = "Please use lower case only.";
+    email.style.border = '1px solid red';
     event.preventDefault();
-    }
+  } else {
+    form.submit();
+    email.remove('style');
+    msg.remove();
+  }
 });
