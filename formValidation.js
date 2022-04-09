6280b0ed4bfcaad933d578a13efbac7cd156fdd1
marurevi <maryrev@hotmail.com>
2022-04-09 02:48:51 -0300

    fix linters

--- formValidation.js
+++ formValidation.js
@@ -1,9 +1,10 @@
 const email = document.getElementById('email');
 const msg = document.querySelector('small');
+const form = document.getElementById('submitbtn');
 
 email.addEventListener('submit', (event) => {
-  if(email.value !== email.value.toLowerCase()){
-    msg.textContent = "Please use lower case only.";
+  if (email.value !== email.value.toLowerCase()) {
+    msg.textContent = 'Please use lower case only.';
     email.style.border = '1px solid red';
     event.preventDefault();
   } else {
