2d36733ec72ef924530acbcb84cef4da771251ee
marurevi <maryrev@hotmail.com>
2022-05-05 18:56:17 -0300

    Fix linters

--- popupWindows.js
+++ popupWindows.js
@@ -114,6 +114,6 @@ btnWhite.forEach((button) => {
   button.addEventListener('click', (event) => {
     button.setAttribute('id', `${event.path[1].classList[0]}`);
     popupContainer.classList.remove('hidden');
-    fillContainer(button.id)
+    fillContainer(button.id);
   });
 });
